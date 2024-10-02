28f224d4d2a994ec9b7162af9560e17c59d6206e
ifef228 <ifef228@gmail.com>
2024-10-02 10:10:58 +0300

    6 задача

---
+++ lab2/cm_timer.py
@@ -0,0 +1,27 @@
+from time import time, sleep
+from contextlib import contextmanager
+
+
+class CmTimer1:
+    def __enter__(self):
+        self.start = time()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print(time() - self.start)
+
+
+@contextmanager
+def cm_timer2():
+    start_time = time()
+    yield
+    end_time = time()
+    print(end_time - start_time)
+
+
+with CmTimer1():
+    sleep(5.5)
+
+with cm_timer2():
+    sleep(1.2)
+
+
